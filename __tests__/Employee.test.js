16bccb4a77d6c0a64cf6ddd02d746fccbdc1e6ca
GonzalezL13 <luisgonzalez1331@icloud.com>
2020-08-01 20:19:55 -0700

    employee and engineer test created with constructors

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,15 @@
+const Employee = require("../lib/Employee");
+
+test("creates an employee object", () => {
+    const employee = new Employee("Luis", 100, "luisg@fake.com");
+
+    expect(employee.name).toEqual(expect.any(String))
+    expect(employee.id).toEqual(expect.any(Number))
+    expect(employee.email).toEqual(expect.any(String))
+});
+
+test("getRole() returns as 'Employee", () => {
+    const employee = new Employee("Luis", 100, "luisg@fake.com");
+
+    expect(employee.getRole()).toEqual(expect.any(String))
+});
---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,13 @@
+const Engineer = require("../lib/Engineer");
+
+test("create and engineer object", () => {
+    const engineer = new Engineer("Luis", 100, "luisg@fake.com", "Gon13");
+
+    expect(engineer.github).toEqual(expect.any(String))
+});
+
+test("getRole() returns as 'Engineer", () => {
+    const engineer = new Engineer("Luis", 100, "luisg@fake.com", "Gon13");
+
+    expect(engineer.getRole()).toEqual(expect.any(String))
+});
